927d74d4b6a01bd09c0eeb5f2f1166ab6b8437f4
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-05-05 00:04:45 +0900

    some fix

--- bin/build.js
+++ bin/build.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const fetch = require('node-fetch')
 const zlib = require('zlib');
-const PNG = require('pngjs').PNG
 
 const { csv2json } = require('./lib/csv2json')
 const { isFile } = require('./lib/isfile')
+const { transcode } = require('./lib/transcode')
 
 const publicdir = `${path.dirname(__dirname)}/tiles`
 fs.mkdirSync(publicdir, {recursive: true})
@@ -38,35 +38,11 @@ fetch(mokurokuURL)
             const tileUrl = `${url.replace(/\/$/, '')}/${tile}`
             const res = await fetch(tileUrl)
             const buffer = await res.buffer();
-            const png = PNG.sync.read(buffer);
 
-            for (let y = 0; y < png.height; y++) {
-              for (let x = 0; x < png.width; x++) {
-                let i = (png.width * y + x) * 4
-                let d = png.data[i] * 2 ** 16 +
-                  png.data[i + 1] * 2 ** 8 +
-                  png.data[i + 2]
-                let h = (d < 2 ** 23) ? d : d - 2 ** 24
-                if (h == - (2 ** 23)) {
-                  h = 0
-                } else {
-                  h *= 0.01
-                }
-
-                let box = Math.round(10 * (h + 10000)).toString(16)
-                let boxr = parseInt(box.slice(-6, -4), 16)
-                let boxg = parseInt(box.slice(-4, -2), 16)
-                let boxb = parseInt(box.slice(-2), 16)
-
-                png.data[i] = boxr
-                png.data[i + 1] = boxg
-                png.data[i + 2] = boxb
-              }
-            }
+            const pngbuffer = transcode(buffer)
 
             const filename = path.join(publicdir, tile)
             fs.mkdirSync(path.dirname(filename), {recursive: true})
-            const pngbuffer = PNG.sync.write(png)
             fs.writeFileSync(filename, pngbuffer)
 
             fs.appendFileSync(log, newData[tile].join(',') + "\n")
